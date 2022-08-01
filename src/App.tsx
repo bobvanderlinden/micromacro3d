37209299d68a40d31d1a0a54756762b2b841acc6
Bob van der Linden <bobvanderlinden@gmail.com>
2022-08-02 00:07:10 +0200

    light

--- src/App.tsx
+++ src/App.tsx
@@ -95,6 +95,7 @@ function App() {
           intensity={0.21}
           position={new Vector3(-1, 1.75, 1).multiplyScalar(10)}
         />
+        <pointLight intensity={0.2} position={new Vector3(5, 10, 0)} />
         <ModelGallery modelUrls={roadTileUrls} />
       </scene>
     </Canvas>
